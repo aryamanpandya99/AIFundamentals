2ca7b32716ad823392da1d238a8efa1cfb8e415d
aryamanpandya99 <aryaman.pandya@tufts.edu>
2022-11-11 14:10:11 -0500

    Chose problem to follow along

---
+++ P1DynamicProgramming/APSP.cpp
@@ -0,0 +1,36 @@
+/*
+All-Pairs Shortest Path 
+Using Dynamic programming to find the shortest path between nodes in 
+an adjacency matrix 
+
+
+This project (along with most of the others in this repo) are extremely basic problems as a 
+CS and AI techniques refresher as I transition between one advanced class (Computer Vision)
+and another (either NLP or Reinforcement learning)
+
+The goal is not to learn the algorithms the best, but to re-familiarize myself with certain 
+techniques 
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+int shortest_path(int n, vector<vector<int>> adjMatrix)
+{
+    vector<vector<int>> cost(n, vector<int>(n)); 
+}
+
+int main()
+{
+    vector<vector<int>> adj_matrix
+    {
+        {0, -1, -2, 0},
+        {4, 0, 2, 4},
+        {5, 1, 0, 2}, 
+        {3, -1, 1, 0}
+    };
+
+    
+}
