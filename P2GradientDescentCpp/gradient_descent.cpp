3004563db0d77a5c94e370337e0dd6ea97509064
aryamanpandya99 <aryaman.pandya@tufts.edu>
2022-11-16 10:43:16 -0500

    Add vector math helper functions

--- P2GradientDescentCpp/gradient_descent.cpp
+++ P2GradientDescentCpp/gradient_descent.cpp
@@ -5,13 +5,10 @@ a linear regression model in c++
 
 #include <iostream>
 #include <vector>
+#include "matplotlib.h"
 
-using namespace std; 
-
-void linear_regression(int epochs, float alpha, std::vector<float> x, std::vector<float> y)
-{
-
-}
+using namespace std;
+namespace plt = matplotlibcpp;  
 
 template<typename T>
 void print_vector(vector<T> const& arr)
@@ -22,6 +19,88 @@ void print_vector(vector<T> const& arr)
 
     }
     cout <<endl; 
+}
+
+vector<float> multiply_vectors(vector<float> a, vector<float> b)
+{
+    vector<float> solution; 
+    int n = a.size(); 
+    if(a.size() != b.size())
+    {
+        cerr<< "Vectors must be the same length to be multiplied"<<endl;
+        return  solution; 
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        solution.push_back(a[i]*b[i]); 
+    }
+
+    return solution; 
+}
+
+vector<float> add_vectors(vector<float> a, vector<float>)
+{
+    vector<float> solution; 
+    int n = a.size(); 
+    if(a.size() != b.size())
+    {
+        cerr<< "Vectors must be the same length to be added"<<endl;
+        return  solution; 
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        solution.push_back(a[i]+b[i]); 
+    }
+
+    return solution; 
+}
+
+//a - b
+vector<float> sub_vectors(vector<float> a, vector<float>)
+{
+    vector<float> solution; 
+    int n = a.size(); 
+    if(a.size() != b.size())
+    {
+        cerr<< "Vectors must be the same length to be subtracted"<<endl;
+        return  solution; 
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        solution.push_back(a[i]-b[i]); 
+    }
+
+    return solution; 
+}
+
+vector<float> mult_scalar_vector(float a, vector<float> vec)
+{
+    vector<float> solution; 
+    int n = a.size(); 
+    
+    for (int i = 0; i < n; i++)
+    {
+        solution.push_back(a+b[i]); 
+    }
+
+    return solution; 
+}
+
+void plot_data (vector<T> x_data, vector<T>y_data)
+{
+    plt::figure_size(224, 168); 
+    plt::plot(x, y); 
+    plt::title("Simple x v/s y plot"); 
+    plt::xlabel("X"); 
+    plt::ylabel("Y"); 
+}
+
+void linear_regression(int epochs, float alpha, std::vector<float> x, std::vector<float> y)
+{
+
 }
 
 int main() 
@@ -49,6 +128,10 @@ int main()
         noise.push_back(r);
 
     }
+    vector<float> dummy1{1, 2, 3}; 
+    vector<float> dummy2{1, 2, 3}; 
+    vector<float> y = multiply_vectors(dummy1, dummy2); 
+    print_vector(y); 
     /*print_vector(x);
     print_vector(noise);
     print_vector(m);
