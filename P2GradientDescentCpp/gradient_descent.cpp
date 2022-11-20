25b4626e67c33f7ac7d269e7eddee02312147e1b
aryamanpandya99 <aryaman.pandya@tufts.edu>
2022-11-20 17:15:21 -0500

    modified:   P2GradientDescentCpp/gd 	modified:   P2GradientDescentCpp/gradient_descent.cpp

--- P2GradientDescentCpp/gradient_descent.cpp
+++ P2GradientDescentCpp/gradient_descent.cpp
@@ -21,6 +21,18 @@ void print_vector(vector<T> const& arr)
     cout <<endl; 
 }
 
+//sum of all elements in a vector 
+float sum_vector(vector<float> const& arr)
+{
+    float output; 
+    for (auto const& element : arr)
+    {
+        output += element; 
+
+    }
+    return output; 
+}
+
 vector<float> multiply_vectors(vector<float> a, vector<float> b)
 {
     vector<float> solution; 
@@ -39,7 +51,7 @@ vector<float> multiply_vectors(vector<float> a, vector<float> b)
     return solution; 
 }
 
-vector<float> add_vectors(vector<float> a, vector<float>)
+vector<float> add_vectors(vector<float> a, vector<float> b)
 {
     vector<float> solution; 
     int n = a.size(); 
@@ -58,7 +70,7 @@ vector<float> add_vectors(vector<float> a, vector<float>)
 }
 
 //a - b
-vector<float> sub_vectors(vector<float> a, vector<float>)
+vector<float> sub_vectors(vector<float> a, vector<float> b)
 {
     vector<float> solution; 
     int n = a.size(); 
@@ -79,11 +91,11 @@ vector<float> sub_vectors(vector<float> a, vector<float>)
 vector<float> mult_scalar_vector(float a, vector<float> vec)
 {
     vector<float> solution; 
-    int n = a.size(); 
+    int n = vec.size(); 
     
     for (int i = 0; i < n; i++)
     {
-        solution.push_back(a+b[i]); 
+        solution.push_back(a*vec[i]); 
     }
 
     return solution; 
@@ -100,7 +112,24 @@ void plot_data (vector<T> x_data, vector<T>y_data)
 
 void linear_regression(int epochs, float alpha, std::vector<float> x, std::vector<float> y)
 {
+    if(x.size()!=y.size(){})
+    {
+        cerr << "X and Y have dimensions, correct and try again" << endl; 
+        return; 
+    }
+    int n = x.size(); 
+    float m_hat = 0, c_hat = 0, coeff = (-2/n); 
+    vector<float> y_hat(n,0), c_hat_vec(n, c_hat), dm(n, 0), dc(n, 0); 
 
+    for(int i = 0; i < epochs; i++)
+    {
+        y_hat = add_vectors(mult_scalar_vector(m_hat, x), c_hat_vec); //y = m*x + c
+        dm = mult_scalar_vector(coeff, sum_vector(multiply_vectors(x, sub_vectors(y, y_hat)))); 
+        dc = mult_scalar_vector(coeff, sum_vector(sub_vectors(y, y_hat))); 
+
+        m_hat = sub_vectors(m_hat, mult_scalar_vector(alpha, dm)); 
+        c_hat = sub_vectors(m_hat, mult_scalar_vector(alpha, dc)); 
+    }
 }
 
 int main() 
@@ -128,10 +157,7 @@ int main()
         noise.push_back(r);
 
     }
-    vector<float> dummy1{1, 2, 3}; 
-    vector<float> dummy2{1, 2, 3}; 
-    vector<float> y = multiply_vectors(dummy1, dummy2); 
-    print_vector(y); 
+
     /*print_vector(x);
     print_vector(noise);
     print_vector(m);
