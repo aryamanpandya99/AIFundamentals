c9aee317c0b5106b7d652b90d0ae7f06f20e559d
aryamanpandya99 <aryaman.pandya@tufts.edu>
2022-12-20 22:58:08 -0500

    Slow day, not too much programming

--- GraphSearchAlgorithms/Djikstra/Djikstra.cpp
+++ GraphSearchAlgorithms/Djikstra/Djikstra.cpp
@@ -4,16 +4,32 @@
 */
 
 #include "Djikstra.hpp"
+#include <queue>
+#include <tuple>
 
 using namespace std; 
 
-int Djikstra::min_distance()
-{
-
-}
+const int INFIN = 1000000000; 
 
 //Djikstra's Algo implementation 
-vector<vector<int>> Djikstra::solution_dkstra()
+int Djikstra::solution_dkstra(dest)
 {
+    vector<bool> visited(n_, false); 
+    vector<int> shortest_path(n_,INFIN); 
+    shortest_path[0] = 0; 
+    int count = 0; 
+    int current_distance = 0; 
+    priority_queue<tuple<int, int>> pq; //tuple element 1 will be vertex, element 2 will be distance  
 
+    while(!visited[dest])
+    {
+        for(int i = 0; i < n_; i++)
+        {
+            if(!visited[i] && adj_matrix_[count][i])
+            {
+                pq.push(make_tuple(i, adj_matrix_[count][i])); 
+            }
+        }
+    
+    }
 }
