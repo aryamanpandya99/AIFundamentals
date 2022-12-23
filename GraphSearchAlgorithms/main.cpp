475036c7a8d8a72c00b88b8b6cb8cea1f253b744
aryamanpandya99 <aryaman.pandya@tufts.edu>
2022-12-23 14:29:19 -0500

    modified:   main.cpp

--- GraphSearchAlgorithms/main.cpp
+++ GraphSearchAlgorithms/main.cpp
@@ -6,31 +6,7 @@
 #include "Djikstra/Djikstra.hpp"
 #include "FloydWarshall/FloydWarshall.hpp"
 
-
-
-
-int main()
-{
-    vector<vector<int>> adj_matrix
-    {
-        {99,   577,  10000, 10},
-        {8975,  0,  3,  7898},
-        {10000, 10000, 0,   7771},
-        {489, 89755, 9097, 70}
-    };
-    
-    cout << "******************Djikstra*********************\n"; 
-    Djikstra djikstra_soln(adj_matrix); 
-    std::cout<<"Input Adjacency Matrix: "<< std::endl; 
-
-    djikstra_soln.printMatrix(adj_matrix);
-
-    cout << "Given destination from source = vertex 4\n";
-    
-    int shortest_path = djikstra_soln.solution_dkstra(4); 
-    std::cout<<"Shortest path to destination: "<<shortest_path<< endl;
-}
-
+using namespace std; 
 
 int main()
 {
@@ -50,4 +26,15 @@ int main()
     std::cout<<"After Optimizing: "<< std::endl;
     std::vector<vector<int>> optimized = apsp_matrix.solution_fw(); 
     apsp_matrix.printMatrix(optimized);
+
+    cout << "******************Djikstra*********************\n"; 
+    Djikstra djikstra_soln(adj_matrix); 
+    std::cout<<"Input Adjacency Matrix: "<< std::endl; 
+
+    djikstra_soln.printMatrix(adj_matrix);
+
+    cout << "Given destination from source = vertex 4\n";
+    
+    int shortest_path = djikstra_soln.solution_dkstra(4); 
+    std::cout<<"Shortest path to destination: "<<shortest_path<< endl;
 }
